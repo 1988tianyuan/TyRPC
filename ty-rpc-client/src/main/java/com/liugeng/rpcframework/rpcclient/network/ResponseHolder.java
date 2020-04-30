9b71a1aefbeb3fe16b4856ac6bae71252c2bde0b
1988tianyuan <467651794@qq.com>
2020-04-30 18:04:14 +0800

    add ResponseHolder

--- ty-rpc-client/src/main/java/com/liugeng/rpcframework/rpcclient/network/ResponseHolder.java
+++ ty-rpc-client/src/main/java/com/liugeng/rpcframework/rpcclient/network/ResponseHolder.java
@@ -3,6 +3,8 @@ package com.liugeng.rpcframework.rpcclient.network;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.liugeng.rpcframework.rpcprotocal.model.RpcResponsePacket;
+
 /**
  * @author Liu Geng liu.geng@navercorp.com
  * @date 2020/4/30 16:35
@@ -11,7 +13,18 @@ public class ResponseHolder {
 	
 	private volatile Map<String, RpcFutureResponse> responseMap = new ConcurrentHashMap<>();
 	
-	public boolean addFutureResponse() {
-		
+	public void addFutureResponse(RpcFutureResponse futureResponse) {
+		responseMap.put(futureResponse.getRequestId(), futureResponse);
+	}
+	
+	public boolean addResponse(RpcResponsePacket response) {
+		String requestId = response.getRequestId();
+		RpcFutureResponse futureResponse = responseMap.get(requestId);
+		if (futureResponse != null) {
+			futureResponse.setResponse(response);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
